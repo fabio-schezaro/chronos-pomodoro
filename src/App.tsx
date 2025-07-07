fa1b5084a62254e05554f9908133dbb0080db942
Fábio Schezaro <fabio_luis95@hotmail.com>
2025-07-06 23:32:03 -0300

    Estrutura CSS e HTML

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,32 @@
-import { Heading } from './components/Heading';
-
 import './styles/theme.css';
 import './styles/global.css';
 
 export function App() {
   return (
     <>
-      <Heading>Olá mundo!</Heading>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus illo
-        velit animi eius rem, totam blanditiis perspiciatis sint soluta fugit
-        laboriosam commodi nihil, obcaecati ipsa quae, unde nulla id eveniet.
-      </p>
+      <div className='container'>
+        <div className='content'>
+          <section>LOGO</section>
+        </div>
+      </div>
+
+      <div className='container'>
+        <div className='content'>
+          <section>MENU</section>
+        </div>
+      </div>
+
+      <div className='container'>
+        <div className='content'>
+          <section>FORM</section>
+        </div>
+      </div>
+
+      <div className='container'>
+        <div className='content'>
+          <section>FOOTER</section>
+        </div>
+      </div>
     </>
   );
 }
