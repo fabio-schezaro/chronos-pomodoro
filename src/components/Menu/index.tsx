20b23312bc5c3166c86cc09b17cb67fae084c03f
Fábio Schezaro <fabio_luis95@hotmail.com>
2025-07-09 22:40:11 -0300

    localStorage e ícones para o theme

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,41 +1,42 @@
-import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from 'lucide-react';
+import {
+  HistoryIcon,
+  HouseIcon,
+  MoonIcon,
+  SettingsIcon,
+  SunIcon,
+} from 'lucide-react';
 import styles from './styles.module.css';
 import { useState, useEffect } from 'react';
 
 type AvailableThemes = 'dark' | 'light';
 
 export function Menu() {
-  const [theme, setTheme] = useState<AvailableThemes>('dark');
+  const [theme, setTheme] = useState<AvailableThemes>(() => {
+    const storageTheme =
+      (localStorage.getItem('theme') as AvailableThemes) || 'dark';
+    return storageTheme;
+  });
+
+  const nextThemeIcon = {
+    dark: <SunIcon />,
+    light: <MoonIcon />,
+  };
 
   function handleThemeChange(
     event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
   ) {
-    event.preventDefault(); //Não segue o link
+    event.preventDefault();
 
     setTheme(prevTheme => {
       const nextTheme = prevTheme === 'dark' ? 'light' : 'dark';
       return nextTheme;
     });
-
-    // document.documentElement.setAttribute('data-theme', theme);
   }
 
-  // useEffect(() => {
-  //   console.log('useEffect sem dependência', Date.now());
-  // }); //Executado toda vez que o componente renderiza na tela.
-
-  // useEffect(() => {
-  //   console.log('useEffect com array de dependência vazio', Date.now());
-  // }, []); //Executa apenas quando o React monta o componente na tela pela primeira vez.
-
   useEffect(() => {
-    console.log('', Date.now());
     document.documentElement.setAttribute('data-theme', theme);
-
-    return () => {
-      console.log('Olha, este componente será atualizado');
-    };
-  }, [theme]); //Executa apenas quando o valor de theme muda.
+    localStorage.setItem('theme', theme);
+  }, [theme]);
 
   return (
     <nav className={styles.menu}>
@@ -73,7 +74,7 @@ export function Menu() {
         title='Mudar Tema'
         onClick={handleThemeChange}
       >
-        <SunIcon />
+        {nextThemeIcon[theme]}
       </a>
     </nav>
   );
