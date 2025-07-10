1366106aa42341dadaa0ecf664a624acb7ed1bbe
Fábio Schezaro <fabio_luis95@hotmail.com>
2025-07-09 21:14:44 -0300

    Programação reativa/useState

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,33 @@ import { Cycles } from './components/Cycles';
 import { DefaultButton } from './components/DefaultButton';
 import { Footer } from './components/Footer';
 import { PlayCircleIcon, StopCircleIcon } from 'lucide-react';
+import { Heading } from './components/Heading';
+import { useState } from 'react';
 
 import './styles/theme.css';
 import './styles/global.css';
 
 export function App() {
+  //Sempre que usar useState, não usar atribuição diretamente.
+
+  // const [numero, setNumero] = useState(() => {
+  //   console.log('Lazy initialization');
+  //   return 0;
+  // });
+
+  const [numero, setNumero] = useState(0);
+
+  //Função reativa "handle"
+  function handleClick() {
+    setNumero(prevState => prevState + 1);
+  }
+
   return (
     <>
+      <Heading>Número: {numero}</Heading>
+      {/* handleClick não tem () pois será ativada no clique do botão */}
+      <button onClick={handleClick}>Aumenta</button>
+
       <Container>
         <Logo />
       </Container>
@@ -30,7 +50,8 @@ export function App() {
         <form className='form' action=''>
           <div className='formRow'>
             <DefaultInput
-              labelText='Tarefa'
+              // labelText='Tarefa'
+              labelText={numero.toString()}
               id='meuInput'
               type='text'
               placeholder='Digite algo'
