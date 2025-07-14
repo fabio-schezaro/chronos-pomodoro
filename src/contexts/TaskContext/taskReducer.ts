43f6f37127cf4296c6b137e422071dd4a398166f
Fábio Schezaro <fabio_luis95@hotmail.com>
2025-07-13 23:22:40 -0300

    TypeScript + Reducer

---
+++ src/contexts/TaskContext/taskReducer.ts
@@ -0,0 +1,20 @@
+import type { TaskStateModel } from '../../models/TaskStateModel';
+import { TaskActionTypes, type TaskActionModel } from './taskAction';
+
+// Sempre deve retornar o estado
+export function taskReducer(
+  state: TaskStateModel,
+  action: TaskActionModel,
+): TaskStateModel {
+  switch (action.type) {
+    case TaskActionTypes.START_TASK: {
+      return state;
+    }
+    case TaskActionTypes.INTERRUPT_TASK: {
+      return state;
+    }
+    case TaskActionTypes.RESET_STATE: {
+      return state;
+    }
+  }
+}
