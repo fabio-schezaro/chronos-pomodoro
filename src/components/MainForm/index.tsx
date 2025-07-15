ecbb281047b8b6f4866c92f04d9ca3ca1fce5436
Fábio Schezaro <fabio_luis95@hotmail.com>
2025-07-14 21:50:26 -0300

    Singleton para gerenciar worker (TimerWorkerManager)

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -9,6 +9,7 @@ import { getNextCycle } from '../../utils/getNextCycle';
 import { getNextCycleType } from '../../utils/getNextCycleType';
 import { TaskActionTypes } from '../../contexts/TaskContext/taskAction';
 import { Tips } from '../Tips';
+import { TimerWorkerManager } from '../../workers/timerWorkerManager';
 
 export function MainForm() {
   const { state, dispatch } = useTaskContext();
@@ -41,6 +42,14 @@ export function MainForm() {
     };
 
     dispatch({ type: TaskActionTypes.START_TASK, payload: newTask });
+
+    const worker = TimerWorkerManager.getInstance();
+
+    worker.postMessage('Olá mundo!');
+
+    worker.onmessage(event => {
+      console.log('PRINCIPAL recebeu:', event.data);
+    });
   }
 
   function handleInterruptTask() {
