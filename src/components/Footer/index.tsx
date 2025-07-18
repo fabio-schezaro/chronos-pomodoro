b07507aa42d7c305fe1bcb36ea5a5f0c85fb88ad
Fábio Schezaro <fabio_luis95@hotmail.com>
2025-07-18 20:47:59 -0300

    RouterLink - criando próprio link para o React Router

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,13 +1,15 @@
-import { Link } from 'react-router';
 import styles from './styles.module.css';
+import { RouterLink } from '../RouterLink';
 
 export function Footer() {
   return (
     <footer className={styles.footer}>
-      <Link to='/about-pomodoro/'>Entenda a técnica pomodoro 🍅</Link>
-      <Link to='/'>
+      <RouterLink href='/about-pomodoro/'>
+        Entenda a técnica pomodoro 🍅
+      </RouterLink>
+      <RouterLink href='/'>
         Chronos Pomodoro &copy; {new Date().getFullYear()} - Feito com 💚
-      </Link>
+      </RouterLink>
     </footer>
   );
 }
