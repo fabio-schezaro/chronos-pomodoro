dc096a36e07b858fc56f83a4a88d7c0b4d009b08
Fábio Schezaro <fabio_luis95@hotmail.com>
2025-07-17 22:26:15 -0300

    React Router - iniciando roteamento da aplicação

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,12 +1,13 @@
+import { Link } from 'react-router';
 import styles from './styles.module.css';
 
 export function Footer() {
   return (
     <footer className={styles.footer}>
-      <a href=''>Entenda a técnica pomodoro 🍅</a>
-      <a href=''>
+      <Link to='/about-pomodoro/'>Entenda a técnica pomodoro 🍅</Link>
+      <Link to='/'>
         Chronos Pomodoro &copy; {new Date().getFullYear()} - Feito com 💚
-      </a>
+      </Link>
     </footer>
   );
 }
