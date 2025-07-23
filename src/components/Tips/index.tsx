748749237b9112ca0a87df74f8a32ffec686ffc7
Fábio Schezaro <fabio_luis95@hotmail.com>
2025-07-22 21:57:02 -0300

    Dispatch de CHANGE_SETTINGS para salvar configurações

--- src/components/Tips/index.tsx
+++ src/components/Tips/index.tsx
@@ -9,15 +9,15 @@ export function Tips() {
 
   //Tips
   const tipsForWhenActiveTask = {
-    workTime: <span>Foque por {state.config.workTime}min</span>,
-    shortBreakTime: <span>Descanse por {state.config.shortBreakTime}min</span>,
+    workTime: <span>Foque por {state.config.workTime} min</span>,
+    shortBreakTime: <span>Descanse por {state.config.shortBreakTime} min</span>,
     longBreakTime: <span>Descanso longo</span>,
   };
 
   const tipsForNoActiveTask = {
-    workTime: <span>Próximo ciclo é de {state.config.workTime}min</span>,
+    workTime: <span>Próximo ciclo é de {state.config.workTime} min</span>,
     shortBreakTime: (
-      <span>Próximo descanso é de {state.config.shortBreakTime}min</span>
+      <span>Próximo descanso é de {state.config.shortBreakTime} min</span>
     ),
     longBreakTime: <span>Próximo descanso longo</span>,
   };
